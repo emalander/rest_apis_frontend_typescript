531f8cdf502c8110c7995caa0cec909c5a2c89d2
Emanuel <ecarabajal@novakorp.io>
2025-07-17 12:34:06 -0300

    se quito createRoot

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { hydrateRoot } from 'react-dom/client';
 
 import LoadingSpinner from './components/LoadingSpinner';
 import { StrictMode, Suspense } from 'react'
-import { createRoot } from 'react-dom/client'
 import { router } from './router'
 import { RouterProvider } from 'react-router-dom' 
 import './index.css'
